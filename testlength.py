f9aa5102957a0c93610f4b88fb938aba7a56b20b
Aphiphu <70745633+Aphiphu@users.noreply.github.com>
2022-12-09 07:16:35 +0700

    Update testlength.py

--- testlength.py
+++ testlength.py
@@ -1,45 +1,55 @@
 #!/usr/bin/env python
 import rospy
 import mediapipe
+from cvzone.HandTrackingModule import HandDetector
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import imutils
 import cv2
+import numpy as np
+import math
 
 def showImage(img):
     cv2.imshow('image', img)
     cv2.waitKey(1)
 
 def callback(msg):
-    try:   
-        while true:
-          bridge = CvBridge()
-          orig = bridge.imgmsg_to_cv2(msg, "bgr8")
-          drawImg = orig
-          resized = cv2.resize(frame, (640, 480))
-          hands, img = detector.findHands(resized)
-          
-          if hands:
-            lmlist = hands[0]["lmlist"][:2]
-            x1, y1 = lmlist[5]
-            x2, y2 = lmlist[17]
-            
-            handdistance = math.sqrt((y2-y1) ** 2 + (x2-x1) ** 2)
-            rospy.loginfo(handdistance)
-            
-    except Exception as err:
-    	print (err)
     
-    showImage(drawImg)
+          try:   
+          
+              bridge = CvBridge()
+              orig = bridge.imgmsg_to_cv2(msg, "bgr8")
+              drawImg = orig
+              resized = cv2.resize(orig, (640, 480))
+              detector = HandDetector(detectionCon=0.8, maxHands=1) 
+              hands, img = detector.findHands(resized)
+             
+          
+              if hands: 
+                 
+                 rospy.loginfo("Detected")
+    
+                 print(hands[0]["lmList"])
+                 points = hands[0]["lmList"]
+                 x1, y1 = points[5][:2]
+                 x2, y2 = points[17][:2]
+            
+                 handdistance = math.sqrt((y2-y1) ** 2 + (x2-x1) ** 2)
+                 print ("Handdistance currently is ", handdistance)
+
+            
+          except Exception as err:
+    	      print (err)
+    
+          showImage(drawImg)
 
 def start_node():
     rospy.init_node('imagesubscriber')
     rospy.loginfo('image subscriber node started')
-    detector = HandDetector(detectionCon=0.8, maxHands=1)\
     
     rawlength =  []
     reallength = [10, 15, 20, 25, 30, 35, 40, 45, 50]
-    coff = np.polyfit(rawlength, reallength, 2)
+   # coff = np.polyfit(rawlength, reallength, 2)
     
     listener()
 
