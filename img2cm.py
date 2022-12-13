d69fca1147602be464ddae4a331929853e3b1f9b
Aphiphu <70745633+Aphiphu@users.noreply.github.com>
2022-12-13 13:16:12 +0700

    Update img2cm.py

--- img2cm.py
+++ img2cm.py
@@ -1,70 +1,57 @@
 #!/usr/bin/env python
 import rospy
-import mediapipe
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import imutils
 import cv2
 
 def showImage(img):
+    print(img)
     cv2.imshow('image', img)
     cv2.waitKey(1)
 
 def callback(msg):
     try:   
+        print ("at callback now")
+        print (msg)
         bridge = CvBridge()
         orig = bridge.imgmsg_to_cv2(msg, "bgr8")
         drawImg = orig
-        resized = cv2.resize(orig, (640, 480))
-        hands, img = detector.findHands(resized)
-          
-        if hands:
-           #check if its an open palm function insert here
-           lmlist = hands[0]["lmlist"]
-           x1, y1 = lmlist[5][:2]
-           x2, y2 = lmlist[17][:2]
-            
-           handdistance = math.sqrt((y2-y1) ** 2 + (x2-x1) ** 2)
-           #handdistanceCM = int(coff[0] * handdistance **2 + coff[1] * handdistance + coff[2])
-            
-        distancetalker(handdistanceCM)
-            
-            
-           
-            
-            
-            
+        #resize image (half-size) for easier processing
+        resized = cv2.resize(orig, None, fx=0.5, fy=0.5)
+        drawImg = resized
+        gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+        # test this
+        blurFrame = cv2.GaussianBlur(grat, (blurRegion, blurRegion), 0)
+        # changes ends here
+        drawImg = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+        
+        edged = cv2.Canny(gray, 35, 125)
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        c = max(cnts, key = cv2.contourArea)
+
     except Exception as err:
     	print (err)
-    
     showImage(drawImg)
+    print ("lol")
 
 def start_node():
-    rospy.init_node('imagesubscriber')
+    rospy.init_node('image_subscriber')
     rospy.loginfo('image subscriber node started')
-    detector = HandDetector(detectionCon=0.8, maxHands=1)\
-    
-    rawlength = []
-    reallength = []
-    coff = np.polyfit(rawlength, reallength, 2)
-    
     listener()
 
 def listener():
-    rospy.Subscriber("cv_camera/image_raw", Image, callback)
-    
+    print("subscriber node running")
+    try:
+        print ("pre") 
+        rospy.Subscriber("/cv_camera/image_raw", Image, callback)
+        print ("post")
+    except Exception:
+        print ("Failed here")
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
-def distancetalker(distance):
-    pub = rospy.Publisher('distanceCM', int, queue_size=10)
-    rospy.init_node('distancetalker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        rospy.loginfo(distance)
-        pub.publish(distance)
-        rate.sleep()
-       
 
 if __name__ == '__main__':
     try:
