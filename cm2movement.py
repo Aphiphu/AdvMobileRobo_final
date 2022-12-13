e1a541da5449f8d8b8053da256399515938d0ac7
Aphiphu <70745633+Aphiphu@users.noreply.github.com>
2022-12-13 13:17:14 +0700

    Update cm2movement.py

--- cm2movement.py
+++ cm2movement.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from std_msgs.msg import Int16
 from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
 import rospy
 import mediapipe
 from cvzone.HandTrackingModule import HandDetector
@@ -11,42 +12,44 @@ import cv2
 import numpy as np
 import math
 
-goalarr = [0]
+
 
 def callback1(data):
     print ("callback1 started")
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", str(data.data))
     distanceCM = data.data
-    goalarr[0] = distanceCM
     
-def callback2(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", str(data.data))
-    distanceCM = goalarr[0]
-    curx = data.data 
-    cury = data.data
-    orient = data.data
-    # function to convert the distanceCM to point (x, y) on the map
-    # this requires the current postion and orientation of the robot
-    # /odom topic has drift which when using map will be transformed for /map frame which is then transformed into the /base frame    
+    goal_publisher = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=5)
+
+    goal = PoseStamped()
+
+    goal.header.seq = 1
+    goal.header.stamp = rospy.Time.now()
+    goal.header.frame_id = "map"
+
+    goal.pose.position.x = (distanceCM/100.0)
+    goal.pose.position.y = 0.0 
+    goal.pose.position.z = 0.0
+
+    goal.pose.orientation.x = 0.0
+    goal.pose.orientation.y = 0.0
+    goal.pose.orientation.z = 0.0
+    goal.pose.orientation.w = 1.0
+
+    rospy.sleep(1)
+    print(goal)
     
-def distance2point(distanceCM, currentx, currenty, orientation):
-    goalx = currentx + distanceCM
-    goaly = currenty + distanceCM 
-    goal = [goalx, goaly]  
-    return goal
+    goal_publisher.publish(goal)
     
     
+       
 def listener():
     
     rospy.init_node('distancelistener', anonymous=True)
-    print ("callback1")
+    print ("callback")
     rospy.Subscriber('distanceCM', Int16, callback1)
-    print ("callback2")
-    rospy.Subscriber('base', String, callback2)
     
-
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-
+    
 if __name__ == '__main__':
     listener()
